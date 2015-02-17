4bf98c19409c248ec988e4d5033e5787fa3ef764
Oliver <o.lucas@live.com>
2015-02-17 18:04:13 +0000

    reviews function working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,13 @@
 class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @reviews = Review.new
+    @review = Review.new
   end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @restaurant.reviews.create(review_params)
+    redirect_to restaurants_path
   end
 
   def review_params
