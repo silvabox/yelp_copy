d1b9cde330e391b73950ef386f53456fe1bab9fd
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-18 17:25:29 +0000

    devise and omniauth added. Bug. Have removed auth/secret for commit, need reentering

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :ominauth_callbacks => "users/ominauth_callbacks"}
   root to: 'restaurants#index'
+
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_sessions
+  end
 
   resources :restaurants do
     resources :reviews
