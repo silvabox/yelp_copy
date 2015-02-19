3d02f2ff22ebf06797bb5e11dc9310b8608f02f6
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-19 16:38:04 +0000

    issues with facebook signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.infor.email
+      user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.image = auth.info.image
