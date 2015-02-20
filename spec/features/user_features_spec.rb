c8c4c4dfc2cb8a14699fcfc70ddf52f4cd709b2d
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-20 12:32:45 +0000

    writing tests - users should not be able to edit or delete restaurants they didn't create

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -39,10 +39,10 @@ context "user signed in on the homepage" do
 end 
 
 
-context "things only users should be able to do" do
+context "users should be logged in" do
   before do
-      Restaurant.create(name: 'KFC')
-    end
+    Restaurant.create(name: 'KFC')
+  end
 
   it "should not let someone who is not logged in create a restaurant" do
     visit '/'
@@ -56,4 +56,37 @@ context "things only users should be able to do" do
     expect(page).to have_content 'Log in'
   end
 
+end
+
+context 'changing restaurant data' do
+
+  before do
+    visit '/'
+    click_link 'Sign up'
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+    click_link 'Add a restaurant'
+    fill_in 'Name', with: 'KFC'
+    click_button 'Create Restaurant'
+    click_button('Sign out')
+    click_link 'Sign up'
+    fill_in('Email', with: 'alt_test@example.com')
+    fill_in('Password', with: 'alttest')
+    fill_in('Password confirmation', with: 'alttest')
+    click_button('Sign up')
+  end
+
+  it 'a user should not be able to edit a restaurant created by another user' do
+    click_link 'Edit KFC'
+    fill_in 'Name', with: 'Kentucky Fried Chicken'
+    click_button 'Update Restaurant'
+    expect(page).not_to have_content 'Kentucky Fried Chicken'
+  end
+
+  it 'a user should not be able to delete a restaurant created by another user' do
+    click_link 'Delete KFC'
+    expect(page).to have_content 'KFC'
+  end
 end
